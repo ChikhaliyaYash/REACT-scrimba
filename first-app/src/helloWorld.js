7d140c4d3a45724753861ef91391e1f7506fe808
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2023-05-22 00:16:51 +0530

    one simple page create

--- first-app/src/helloWorld.js
+++ first-app/src/helloWorld.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import  ReactDOM from 'react-dom'
 
 function HelloWorld() {
   return (
-    <div>HelloWorld</div>
+    <div>
+      <img src="./react-logo.png" width="40px" alt=""/>
+      <h1>Fun facts about React </h1>
+      <ul>
+        <li>Was first released in 2013 </li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained br Facebook</li>
+        <li>Power thousands of enterprise apps, including moblile app</li>
+      </ul>
+      
+
+    </div>
+    
   )
 }
 

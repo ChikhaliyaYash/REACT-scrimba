9f4109e9078f01667d6fd5442f610a2256055111
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2023-06-15 22:27:24 +0530

    simple profile

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -2,17 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import helloWorld from "./helloWorld";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <helloWorld/>
-  </React.StrictMode>
-);
+root.render(<App />);
+
+// ReactDOM.render(<helloWorld/> , document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
